17018c591ddaf520aa472dc8afa0d48b98c5d269
sufifisas <sufifisas@gmail.com>
2023-09-29 22:08:07 +0800

    feat: added express setup

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+require('dotenv').config();
+
 const { Sequelize, DataTypes } = require('sequelize');
 const dbConfig = require('./app/config/database.config');
 const models = require('./app/models');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT;
+
 
 // connect database
 const sequelize = dbConfig(Sequelize);
 
 //define models
 models(sequelize, DataTypes);
+
+app.get('/', (req, res) => {
+    res.send('Hello Sequelize tutorial')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at PORT ${PORT}`)
+})
